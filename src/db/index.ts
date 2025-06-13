7177b147fa56052d42e87f7aab6e7cf45f00b49c
Wolf Arif Mardin <wolf.mardin@gmail.com>
2025-06-13 22:15:42 +0200

    Remove logs

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,16 +5,11 @@ import { Pool } from 'pg'
 import * as schema from './schema'
 import config from '../config/config'
 
-console.log('DB URL:', config.databaseUrl)
-
 const pool = new Pool({
   connectionString: config.databaseUrl,
   ssl: {
     rejectUnauthorized: false,
   },
 })
-
-console.log('Pool ssl config:', pool.options.ssl)
-console.log('Pool connectionString:', pool.options.connectionString)
 
 export const db = drizzle(pool, { schema })

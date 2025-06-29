3150fef32c52cef9c31102a859660a345d547e20
Wolf Arif Mardin <wolf.mardin@gmail.com>
2025-06-29 12:50:48 +0200

    Update drizzle config to just use the db URL string instead of partially parsing it. This is simpler and also includes sslmode in the URL (important for dev on Mac as it defaults to true which we don't want in dev)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,13 @@
 import type { Config } from 'drizzle-kit'
-import config from './src/config/config'
 
-const dbUrl = new URL(config.databaseUrl)
-const dbName = dbUrl.pathname.slice(1)
+// local imports
+import config from './src/config/config'
 
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: dbUrl.hostname,
-    port: Number(dbUrl.port) || 5432,
-    user: dbUrl.username,
-    password: dbUrl.password,
-    database: dbName,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+	url: config.databaseUrl,
   },
-} satisfies Config
+} as Config
